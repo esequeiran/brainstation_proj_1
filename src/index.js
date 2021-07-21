37e5592d0a3f32e29399c9ef3f418e8cbae63d96
esequeiran <esequeiran@ucenfotec.ac.cr>
2021-07-20 20:36:14 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/main.scss";
+
+// let iteration = 0;
+
+// setInterval(() => {
+//   const heading = document.querySelector(".heading");
+//   heading.textContent = `Webpack Template ${iteration}`;
+//   ++iteration;
+// }, 5000);
